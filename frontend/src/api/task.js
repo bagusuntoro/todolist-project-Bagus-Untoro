05a70c7bfab2a9b0e575e917aead1b3cbdf458a0
bagusuntoro <untorobagus65@gmail.com>
2024-09-11 21:54:54 +0700

    fixed

--- frontend/src/api/task.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3000';
-
-export const fetchTasks = () => axios.get(`${API_URL}/list-task`);
-export const createTask = (task) => axios.post(`${API_URL}/create-task`, task);
-export const deleteTask = (taskId) => axios.delete(`${API_URL}/delete-task/${taskId}`);
-export const updateTask = (task) => axios.put(`${API_URL}/update-task/${task.id}`, task);
